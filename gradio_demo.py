279cadef35babf19f3b8b12c48c7cc08dfe71e37
dragonchemist <785524367@qq.com>
2024-11-21 16:36:35 +0800

    x

--- gradio_demo.py
+++ gradio_demo.py
@@ -3,19 +3,16 @@ import torchaudio
 from io import BytesIO
 from viitor_voice.utils.offline_inference import OfflineInference
 
-# 初始化 TTS 引擎
 tts_engine = OfflineInference(
     model_path='ZzWater/viitor-voice-en',
     config_path='viitor_voice/inference_configs/en.json'
 )
 
-# 获取有效说话人列表
 valid_speakers = list(tts_engine.prompt_map.keys())
 
 
-# 定义生成音频函数
 def generate_audio(text, speaker, speed):
-    # 检查说话人是否有效
+    # check if speaker is valid
     if speaker not in valid_speakers:
         return f"Error: Invalid speaker. Please select one from {valid_speakers}."
 
@@ -25,18 +22,17 @@ def generate_audio(text, speaker, speed):
     return audios[0]
 
 
-# Gradio 界面
 with gr.Blocks() as demo:
     gr.Markdown("## Qwen2 based streaming tts")
 
     with gr.Row():
-        text_input = gr.Textbox(label="text", lines=5, placeholder="请输入要转换为语音的文本")
+        text_input = gr.Textbox(label="text", lines=5, placeholder="input text")
         speaker_input = gr.Dropdown(label="speaker", choices=valid_speakers, value=valid_speakers[0])
         speed_input = gr.Slider(label="speed", minimum=0, maximum=7, step=1, value=2)
 
-    output_audio = gr.Audio(label="生成的音频", type="numpy")
+    output_audio = gr.Audio(label="audio", type="numpy")
 
-    generate_button = gr.Button("生成语音")
+    generate_button = gr.Button("generate")
 
     generate_button.click(
         fn=generate_audio,
